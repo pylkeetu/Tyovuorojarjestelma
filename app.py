c0b7abff9035a1d69ec0b13b6b55cb90957402cd
pylkeetu <eetu.pylkkanen@helsinki.fi>
2026-04-15 11:35:22 +0300

    Fixed time to local finnish time on comments

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask_wtf import CSRFProtect
 import config
+from datetime import datetime, timezone, timedelta
 
 from routes.auth import auth_bp
 from routes.shifts import shifts_bp
@@ -7,8 +9,24 @@ from routes.shifts import shifts_bp
 app = Flask(__name__)
 app.secret_key = config.secret_key
 
+csrf = CSRFProtect(app)
+
 app.register_blueprint(auth_bp)
 app.register_blueprint(shifts_bp)
+
+def localtime(value):
+    if value is None:
+        return ""
+
+    if isinstance(value, str):
+        value = datetime.fromisoformat(value)
+
+    finland = timezone(timedelta(hours=3))
+
+    return value.replace(tzinfo=timezone.utc).astimezone(finland).strftime("%d.%m.%Y %H:%M")
+
+
+app.jinja_env.filters["localtime"] = localtime
 
 if __name__ == "__main__":
     app.run(debug=True)
